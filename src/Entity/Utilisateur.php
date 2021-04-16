d826c5510c250a2ba40bbb5e3143ff5c4ca12376
Meinertzhagen Michael <m.meinertzhagen@hainaut-promsoc.be>
2021-04-16 10:55:20 +0200

    Entité utilisateur + rajout notion de verif pass

---
+++ src/Entity/Utilisateur.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UtilisateurRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UtilisateurRepository::class)
+ */
+class Utilisateur
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
+    private $verificationPassword;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+
+    public function getVerificationPassword(): ?string
+    {
+        return $this->verificationPassword;
+    }
+
+    public function setVerificationPassword(string $verificationPassword): self
+    {
+        $this->password = $verificationPassword;
+
+        return $this;
+    }
+}
